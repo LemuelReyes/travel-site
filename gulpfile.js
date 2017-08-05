4e3349f0d79dbabe35553d4533a12bea3940c98b
Lemuel <reyeslemuel@hotmail.com>
2017-08-04 21:29:24 -0700

    separate gulp tasks into partials

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,2 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
-browserSync = require('browser-sync');
-
-gulp.task('styles', function(){
- return gulp.src('./app/assets/styles/styles.css')
- .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
- .pipe(gulp.dest('./app/temp/styles'))
- .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function(){
-
-  browserSync.init({
-          server: "./app"
-      });
-
-  watch('./app/assets/**/*.css', function(){
-    gulp.start('styles');
-  });
-  watch('./app/*html').on('change', browserSync.reload);
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
