7cd9d2284b11f8a5681d517c03e87b4692c9ff9b
Lemuel <reyeslemuel@hotmail.com>
2017-08-16 00:39:49 -0700

    add functionality to mobile menu. mobile menu can now appear and it is styled as well.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-var $ = require('jquery');
+import MobileMenu from './modules/MobileMenu';
 
-import Person from './modules/Person';
-
-class Adults extends Person {
-  payTaxes(){
-    console.log(this.name + " now owes $0 in taxes.");
-  }
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adults("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
